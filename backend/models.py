998bbcd6577e0a2ef2b562229280b9cab9143431
maz1234jac <azeeeem.haider@gmail.com>
2025-05-04 17:41:09 +0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,18 @@
+from pydantic import BaseModel
+
+class MediaUpload(BaseModel):
+    title: str
+    caption: str
+    location: str
+    people: list[str]
+
+class Comment(BaseModel):
+    media_id: str
+    user: str
+    comment: str
+
+class Rating(BaseModel):
+    media_id: str
+    user: str
+    rating: int
 
